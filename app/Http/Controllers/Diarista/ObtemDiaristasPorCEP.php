661dab308799ad235f9fbbd376d5fe7e2f6e285b
Elton Fonseca <elton869@gmail.com>
2021-07-23 13:52:27 -0300

    07 - Conhecendo API Resource

--- app/Http/Controllers/Diarista/ObtemDiaristasPorCEP.php
+++ app/Http/Controllers/Diarista/ObtemDiaristasPorCEP.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Diarista;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\DiaristaPublico;
 use App\Models\User;
 use Illuminate\Http\Request;
 
@@ -16,8 +17,8 @@ class ObtemDiaristasPorCEP extends Controller
      */
     public function __invoke(Request $request)
     {
-        $diaristas = User::where('tipo_usuario', '=', 2)->get();
+        $diarista = User::where('tipo_usuario', '=', 2)->first();
 
-        return $diaristas;
+        return new DiaristaPublico($diarista);
     }
 }
---
+++ app/Http/Resources/DiaristaPublico.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class DiaristaPublico extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'nome_completo' => $this->nome_completo,
+            'reputacao' => $this->reputacao,
+            'foto_usuario' => $this->foto_usuario,
+            'cidade' => 'São Paulo'
+        ];
+    }
+}
