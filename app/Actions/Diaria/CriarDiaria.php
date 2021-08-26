7b1edab400ecef5e5166fa0c6b4698760f44ad4b
Elton Fonseca <elton869@gmail.com>
2021-08-26 18:18:53 -0300

    11 - definir o valor da comissão

--- app/Actions/Diaria/CriarDiaria.php
+++ app/Actions/Diaria/CriarDiaria.php
@@ -3,17 +3,39 @@
 namespace App\Actions\Diaria;
 
 use App\Models\Diaria;
+use App\Models\Servico;
 use Illuminate\Support\Facades\Auth;
 
 class CriarDiaria 
 {
+    /**
+     * Cria a diária no banco de dados
+     *
+     * @param array $dados
+     * @return void
+     */
     public function executar(array $dados)
     {
         $dados['status'] = 1;
         $dados['servico_id'] = $dados['servico'];
-        $dados['valor_comissao'] = 1.00;
+        $dados['valor_comissao'] = $this->calculaComissao($dados);
         $dados['cliente_id'] = Auth::user()->id;
 
         return Diaria::create($dados);
+    }
+
+    /**
+     * Calcula o valor da comissão da plataforma
+     *
+     * @param array $dados
+     * @return float
+     */
+    private function calculaComissao(array $dados): float
+    {
+        $servico = Servico::find($dados['servico_id']);
+
+        $porcentagem = $servico->porcentagem / 100;
+
+        return $dados['preco'] * $porcentagem;
     }
 }
