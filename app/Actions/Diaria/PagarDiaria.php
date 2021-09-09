8350aca2d95a3b8ea558cd9ec9b24d3b1480d48a
Elton Fonseca <elton869@gmail.com>
2021-09-09 16:33:57 -0300

    04 - Valida se o status da diária é igual a 1

--- app/Actions/Diaria/PagarDiaria.php
+++ app/Actions/Diaria/PagarDiaria.php
@@ -3,11 +3,31 @@
 namespace App\Actions\Diaria;
 
 use App\Models\Diaria;
+use Illuminate\Validation\ValidationException;
 
 class PagarDiaria
 {
     public function executar(Diaria $diaria)
     {
+        $this->validaStatusDiaria($diaria);
+
+        //integração com gateway de pagamento
+
         $diaria->pagar();
+    }
+
+    /**
+     * Valida se o status da diária é igual a 1
+     *
+     * @param Diaria $diaria
+     * @return void
+     */
+    private function validaStatusDiaria(Diaria $diaria): void
+    {
+        if ($diaria->status != 1) {
+            throw ValidationException::withMessages([
+                'status-diaria' => 'Só é possível executar essa ação com diarias com status 1'
+            ]);
+        }
     }
 }
