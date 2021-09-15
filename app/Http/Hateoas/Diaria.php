5c4c9a156ac456e9df040731e387ad7227a70d41
Elton Fonseca <elton869@gmail.com>
2021-09-15 17:15:42 -0300

    11 - Adiciona link do show ao heteoas da diária

--- app/Http/Hateoas/Diaria.php
+++ app/Http/Hateoas/Diaria.php
@@ -14,16 +14,28 @@ class Diaria extends HateoasBase implements HateoasInterface
      */
     public function links(?Model $diaria): array
     {
+        $this->adicionaLink('GET', 'self', 'diarias.show', ['diaria' => $diaria->id]);
+
+        $this->linkPagar($diaria);
+
+        return $this->links;
+    }
+
+    /**
+     * Adiciona o link de pagamento na diária
+     *
+     * @param Model $diaria
+     * @return void
+     */
+    private function linkPagar(Model $diaria): void
+    {
         if ($diaria->status == 1) {
             $this->adicionaLink(
-                'POST', 
-                'pagar_diaria', 
-                'diarias.pagar', 
+                'POST',
+                'pagar_diaria',
+                'diarias.pagar',
                 ['diaria' => $diaria->id]
             );
         }
-        
-
-        return $this->links;
     }
 }
