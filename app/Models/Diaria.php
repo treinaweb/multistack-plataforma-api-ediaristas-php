055f8636ff590dd0fecfef23807e64da7d5ef10c
Elton Fonseca <elton869@gmail.com>
2021-09-15 13:34:38 -0300

    04 - Conhecendo o método when

--- app/Models/Diaria.php
+++ app/Models/Diaria.php
@@ -55,10 +55,13 @@ class Diaria extends Model
      */
     static public function todasDoUsuario(User $usuario): Collection
     {
-        if ($usuario->tipo_usuario === 1) {
-            return self::where('cliente_id', $usuario->id)->get();
-        }
-        
-        return self::where('diarista_id', $usuario->id)->get();
+        return self::
+            when($usuario->tipo_usuario === 1, function($q) use($usuario) {
+                $q->where('cliente_id', $usuario->id);
+            })
+            ->when($usuario->tipo_usuario === 2, function($q) use ($usuario){
+                $q->where('diarista_id', $usuario->id);
+            })
+            ->get();
     }
 }
