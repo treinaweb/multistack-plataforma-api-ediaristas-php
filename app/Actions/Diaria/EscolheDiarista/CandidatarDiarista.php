a262f2f00b1fcf664982f2e9d2db5fd9d3aa01da
Elton Fonseca <elton869@gmail.com>
2021-10-20 15:30:50 -0300

    07 - Tipo de usuário e duplicidade.mov

--- app/Actions/Diaria/EscolheDiarista/CandidatarDiarista.php
+++ app/Actions/Diaria/EscolheDiarista/CandidatarDiarista.php
@@ -5,6 +5,7 @@ namespace App\Actions\Diaria\EscolheDiarista;
 use App\Checkers\Diaria\ValidaStatusDiaria;
 use App\Models\Diaria;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Gate;
 use Illuminate\Validation\ValidationException;
 
 class CandidatarDiarista
@@ -16,12 +17,27 @@ class CandidatarDiarista
 
     public function executar(Diaria $diaria)
     {
+        Gate::authorize('tipo-diarista');
         $this->validaStatusDiaria->executar($diaria, 2);
+        $this->verificaDuplicidadeDeCandidato($diaria);
 
         $diaria->candidatas()->create([
             'diarista_id' => Auth::user()->id
         ]);
 
         dd('depois da gravação');
+    }
+
+    private function verificaDuplicidadeDeCandidato(Diaria $diaria)
+    {
+        $diaristaCandidato = $diaria->candidatas()
+            ->where('diarista_id', Auth::user()->id)
+            ->first();
+
+        if ($diaristaCandidato) {
+            throw ValidationException::withMessages([
+                'data_criacao' => 'O/A diarista já é candidato(a) dessa diária'
+            ]);
+        }
     }
 }
