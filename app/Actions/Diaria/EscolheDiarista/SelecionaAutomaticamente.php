53c9308cc9232bb2709d097c50cb5eb0977382ed
Elton Fonseca <elton869@gmail.com>
2021-10-28 13:02:21 -0300

    16 - Escolhendo o melhor diarista para cada diária

--- app/Actions/Diaria/EscolheDiarista/SelecionaAutomaticamente.php
+++ app/Actions/Diaria/EscolheDiarista/SelecionaAutomaticamente.php
@@ -3,18 +3,35 @@
 namespace App\Actions\Diaria\EscolheDiarista;
 
 use App\Models\Diaria;
+use App\Tasks\Diarista\SelecionaDiaristaIndice;
 use Carbon\Carbon;
 
 class SelecionaAutomaticamente
 {
+    public function __construct(
+        private SelecionaDiaristaIndice $selecionaDiaristaIndice
+    ) {
+    }
+
     public function executar()
     {
         $diarias = Diaria::where('status', 2)
             ->where('created_at', '<', Carbon::now()->subHours(24))
+            ->withCount('candidatas')
             ->get();
 
         foreach ($diarias as $diaria) {
-            var_dump($diaria);
+
+            if ($diaria->candidatas_count === 1) {
+                $diaria->confirmar($diaria->candidatas[0]->diarista_id);
+                continue;
+            }
+
+            if ($diaria->candidatas_count > 1) {
+                $diaria->confirmar(
+                    $this->selecionaDiaristaIndice->executar($diaria)
+                );
+            }
         }
     }
 }
