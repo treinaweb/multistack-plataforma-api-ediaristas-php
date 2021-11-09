88761368118398b607f874698b93c41752ef589f
Elton Fonseca <elton869@gmail.com>
2021-11-09 16:04:10 -0300

    09 - Refatorando a action de pagamento da diária

--- app/Actions/Diaria/PagarDiaria.php
+++ app/Actions/Diaria/PagarDiaria.php
@@ -5,6 +5,7 @@ namespace App\Actions\Diaria;
 use App\Checkers\Diaria\ValidaStatusDiaria;
 use App\Models\Diaria;
 use App\Services\Pagamento\PagamentoInterface;
+use App\Services\Pagamento\TransacaoResponse;
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Validation\ValidationException;
 
@@ -25,29 +26,73 @@ class PagarDiaria
      */
     public function executar(Diaria $diaria, string $cardHash): bool
     {
+        $this->realizaValidacoes($diaria);
+
+        $transacao = $this->realizaTrasancaoComGateway($diaria, $cardHash);
+        $this->guardaTransacaoBancoDeDados($diaria, $transacao);
+        $this->validaStatusPagamento($transacao);
+
+        return $diaria->pagar();
+    }
+
+    /**
+     * realiza as validações antes do pagamento
+     *
+     * @param Diaria $diaria
+     * @return void
+     */
+    private function realizaValidacoes(Diaria $diaria): void
+    {
         $this->validaStatusDiaria->executar($diaria, 1);
         Gate::authorize('tipo-cliente');
         Gate::authorize('dono-diaria', $diaria);
+    }
 
-        //integração com gateway de pagamento
-        $transacao = $this->pagamento->pagar([
+    /**
+     * Chama o serviço de pagamento para realizar a transação
+     *
+     * @param Diaria $diaria
+     * @param string $cardHash
+     * @return TransacaoResponse
+     */
+    private function realizaTrasancaoComGateway(Diaria $diaria, string $cardHash): TransacaoResponse
+    {
+        return $this->pagamento->pagar([
             'amount' => intval($diaria->preco * 100),
             'card_hash' => $cardHash,
             'async' => false
         ]);
+    }
 
+    /**
+     * Salva o resultado da transação do gateway no banco de dados
+     *
+     * @param Diaria $diaria
+     * @param TransacaoResponse $transacao
+     * @return void
+     */
+    private function guardaTransacaoBancoDeDados(Diaria $diaria, TransacaoResponse $transacao): void
+    {
         $diaria->pagamentos()->create([
             'status' => $transacao->status === 'paid' ? 'pago' : 'reprovado',
             'transacao_id' => $transacao->transacaoId,
             'valor' => $diaria->preco
         ]);
+    }
 
+    /**
+     * valida se o status da transação é pago
+     * se não for, gera uma exceção
+     *
+     * @param TransacaoResponse $transacao
+     * @return void
+     */
+    public function validaStatusPagamento(TransacaoResponse $transacao): void
+    {
         if ($transacao->status !== 'paid') {
             throw ValidationException::withMessages([
                 'pagamento' => 'Pagamento Reprovado'
             ]);
         }
-
-        return $diaria->pagar();
     }
 }
