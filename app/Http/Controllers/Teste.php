8e5cdbe14f4e539275aae4de74cc25364ca75576
Elton Fonseca <elton869@gmail.com>
2021-11-09 14:10:28 -0300

    04 - Gerando o cardhash e finalizando a trasação

--- app/Http/Controllers/Teste.php
+++ app/Http/Controllers/Teste.php
@@ -19,7 +19,7 @@ class Teste extends Controller
 
         $transaction = $pagarme->transactions()->create([
             'amount' => '2000',
-            'card_hash' => '123123',
+            'card_hash' => '4658941_v0FrQgy3FFy+7a0zYl6ltVMR8PzInnHTaKg/My4wJX1ruPrBTx3gh7+WrHHeAppewmeMdfRdQfierEzkmW7phRv6NT/KQHjEcwBm8pU+OVP+WgWDsVBhzRm7CL6pDIuxS4xaM+bTPomhOcQOQb98YPseJak1iSu2x0/cy1v4a1PItptGbovMOzIUu++1ax284/JKXYuthN1NZqdEcSu6GlPxKv55gmApSCtq5RNJEjs8VRfcq/oGCFAWn6TV8DvJGIXKj58RJxrAqom2AlL3xZdIYwPZCvb/qHaXw5/iHaj+R7tUq1eBzgTbCnE3nvCJvMtqwouoC9V+P6jjBI6hJQ==',
             'async' => false
         ]);
 
