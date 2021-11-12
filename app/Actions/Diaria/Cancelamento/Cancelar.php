80ca125f84de7e19dd1c0ceebced02769a81b2d3
Elton Fonseca <elton869@gmail.com>
2021-11-12 16:44:55 -0300

    03 - Validando mais de um status de diária.mov

--- app/Actions/Diaria/Cancelamento/Cancelar.php
+++ app/Actions/Diaria/Cancelamento/Cancelar.php
@@ -2,12 +2,20 @@
 
 namespace App\Actions\Diaria\Cancelamento;
 
+use App\Checkers\Diaria\ValidaStatusDiaria;
 use App\Models\Diaria;
 
 class cancelar
 {
+    public function __construct(
+        private ValidaStatusDiaria $validaStatusDiaria
+    ) {
+    }
+
     public function executar(Diaria $diaria)
     {
+        $this->validaStatusDiaria->executar($diaria, [1, 2, 3, 6]);
+
         dd($diaria);
     }
 }
--- app/Checkers/Diaria/ValidaStatusDiaria.php
+++ app/Checkers/Diaria/ValidaStatusDiaria.php
@@ -14,12 +14,46 @@ class ValidaStatusDiaria
      * @param integer $status
      * @return void
      */
-    public function executar(Diaria $diaria, int $status): void
+    public function executar(Diaria $diaria, int|array $status): void
     {
-        if ($diaria->status != $status) {
+        if ($this->statusInvalido($diaria, $status)) {
+            $mensagemValidacao = "Só é possível executar essa ação com diarias com status ";
+            $mensagemValidacao .= $this->statusValidos($status);
+
             throw ValidationException::withMessages([
-                'status-diaria' => "Só é possível executar essa ação com diarias com status $status"
+                'status-diaria' => $mensagemValidacao
             ]);
         }
+    }
+
+    /**
+     * Indica se o status da diária é invalido
+     *
+     * @param Diaria $diaria
+     * @param integer|array $status
+     * @return boolean
+     */
+    private function statusInvalido(Diaria $diaria, int|array $status): bool
+    {
+        if (is_int($status)) {
+            return $diaria->status != $status;
+        }
+
+        return !in_array($diaria->status, $status);
+    }
+
+    /**
+     * Formata os status válidos
+     *
+     * @param integer|array $status
+     * @return string
+     */
+    private function statusValidos(int|array $status): string
+    {
+        if (is_int($status)) {
+            return $status;
+        }
+
+        return implode(', ', $status);
     }
 }
