24b5eb66151c1aaf8a0c391f33ffb812670d80fb
Elton Fonseca <elton869@gmail.com>
2021-11-16 11:44:21 -0300

    04 - Validando a data de atendimento

--- app/Actions/Diaria/Cancelamento/Cancelar.php
+++ app/Actions/Diaria/Cancelamento/Cancelar.php
@@ -4,6 +4,9 @@ namespace App\Actions\Diaria\Cancelamento;
 
 use App\Checkers\Diaria\ValidaStatusDiaria;
 use App\Models\Diaria;
+use Carbon\Carbon;
+use Illuminate\Support\Facades\Gate;
+use Illuminate\Validation\ValidationException;
 
 class cancelar
 {
@@ -14,8 +17,23 @@ class cancelar
 
     public function executar(Diaria $diaria)
     {
-        $this->validaStatusDiaria->executar($diaria, [1, 2, 3, 6]);
+        $this->validaStatusDiaria->executar($diaria, [2, 3]);
+        $this->verificaDataAtendimento($diaria->data_atendimento);
+        Gate::authorize('dono-diaria', $diaria);
 
         dd($diaria);
+    }
+
+    private function verificaDataAtendimento(string $dataAtendimento)
+    {
+        $dataAtendimento = new Carbon($dataAtendimento);
+        $agora = Carbon::now();
+
+        if ($agora > $dataAtendimento) {
+            throw ValidationException::withMessages([
+                'data_atendimento' => 'Não é mais possível cancelar a diária.' .
+                    'Entre em contato com nosso suporte'
+            ]);
+        }
     }
 }
